1661b72ce99d33558dcfd326a3bca492b0d4a96c
SAVIGA09 <amore.gabriele090409@gmail.com>
2026-03-27 09:46:53 +0100

    Update main.cpp
    modifica main che sposta i nemici

--- SpaceInvaders_FattoDaPippati/SpaceInvaders_FattoDaPippati/main.cpp
+++ SpaceInvaders_FattoDaPippati/SpaceInvaders_FattoDaPippati/main.cpp
@@ -1,9 +1,31 @@
-#include <iostream>
-
+#include <string>
+#include <windows.h>
+#include <gdiplus.h> //sempre per trasformare i dati in qualcosa che si puo vedere a schermo
+#include "Nemico.h"
 using namespace std;
+using namespace Gdiplus;
 
-int main()
-{
-	cout << "Apuz rivoglio i miei 50cent" << endl;
+int main() {
+	Nemico n1 = Nemico(100, 100, 20, 20, "alieno", Color::Red);
+	Nemico n2 = Nemico(100, 200, 20, 20, "alieno", Color::Blue); //dichiarazione ipotetica degli alieni nemici
+
+	// inizia lo shift automatico degli alieni 
+	//il countspara parte da 0 perche non si sta iniziando a sparare e poi si aggiorna nel while
+	int countSpara = 0;
+
+	while (true) {
+
+		n1.SpostatiESpara();
+		n2.SpostatiESpara();
+
+		Sleep(100);//si rallenta il ciclo 
+
+		//si continua lo shift delle posizione degli alieni in concomitanza al fatto che sparano colpi
+
+		countSpara++;
+		
+	}
+	//serve sfruttare il gothit e poi il successivo unalive per valutare se l'alieno viene eliminato o meno 
+	
 	return 0;
 }
