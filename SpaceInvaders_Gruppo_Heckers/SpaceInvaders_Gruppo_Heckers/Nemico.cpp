dce0054877e9fc99d265fe78c2c7296446c904ea
Gabriele Amore <amore.gabriele090409@gmail.com>
2026-03-29 17:10:39 +0200

    modifica cpp

--- SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/Nemico.cpp
+++ SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/Nemico.cpp
@@ -1,31 +1,80 @@
-#include <string>
-#include <windows.h>
-#include <gdiplus.h> //sempre per trasformare i dati in qualcosa che si puo vedere a schermo
 #include "Nemico.h"
-using namespace std;
-using namespace Gdiplus;
 
-int main() {
-	Nemico n1 = Nemico(100, 100, 20, 20, "alieno", Color::Red);
-	Nemico n2 = Nemico(100, 200, 20, 20, "alieno", Color::Blue); //dichiarazione ipotetica degli alieni nemici
+Nemico::Nemico() {  //inizializzo tutti gli oggetti che andro ad utilizzare 
+    this->x = 0;
+    this->y = 0;
+    this->width = 35;  // Dimensione ipotetica
+    this->height = 40; //dimensione ipotetica
+    this->phase = 0;
+    this->type = " ";
+    this->color = Color::White;
+    this->isAlive = true;
+}
 
-	// inizia lo shift automatico degli alieni 
-	//il countspara parte da 0 perche non si sta iniziando a sparare e poi si aggiorna nel while
-	int countSpara = 0;
+Nemico::Nemico(int x, int y, int height, int width, string type, Color color) {  //punto agli oggetti che ho inizializzato
+    this->x = x;
+    this->y = y;
+    this->height = height;
+    this->width = width;
+    this->phase = 0;
+    this->type = type;
+    this->color = color;
+    this->isAlive = true;
+}
 
-	while (true) {
+void Nemico::GotHit() {
 
-		n1.SpostatiESpara();
-		n2.SpostatiESpara();
+    this->isAlive = false; // Se l'alieno viene colpito lo stato isAlive diventa falso
+}
 
-		Sleep(100);//si rallenta il ciclo 
+bool Nemico::Unalive() {
 
-		//si continua lo shift delle posizione degli alieni in concomitanza al fatto che sparano colpi
+    if (this->isAlive == false) {  //se isAlive e' falso vuol dire che l'alieno e' stato eliminato e quindi unalive e' vero
+        return true;
+    }
+    else {    //senno l'alieno e' ancora vivo
+        return false;
+    }
+}
+void Nemico::ShiftSx(int pixel) {
 
-		countSpara++;
+    this->x -= pixel;   // spostamento verso sinistra e quindi diminuisco la coordinata x di quanti pixel mi ha detto l'utente
+}
 
-	}
-	//serve sfruttare il gothit e poi il successivo unalive per valutare se l'alieno viene eliminato o meno 
+void Nemico::ShiftDx(int pixel) {
 
-	return 0;
+    this->x += pixel; // spostamento verso destra e quindi aggiungo alla coordinata x quanti pixel mi ha detto l'utente
+}
+
+void Nemico::ShiftUp(int pixel) {
+
+    this->y -= pixel;  // spostamento verso l'alto e quindi diminuisco la coordinata y di quanti pixel mi ha detto l'utente
+}
+
+void Nemico::ShiftDown(int pixel) {
+
+    this->y += pixel;    // spostamento verso il basso e quindi aggiungo alla coordinata y quanti pixel mi ha detto l'utente
+}
+
+void Nemico::Shift() {
+    // esempio di automatismo tipo l'alieno cambia fase ogni volta che si muove tipo destra-sinistra la fase cambia
+    //pero da pensare in altre maniere anche senno si sfruttano gli shift ma sono tante piu righe da scrivere
+    this->ChangePhase();
+}
+
+void Nemico::ChangePhase() {
+
+    if (this->phase == 0) {     // alternanza tra fase 0 e fase 1 per l'animazione come per esempio gambe aperte o gambe chiuse
+        this->phase = 1;
+    }
+    else {
+        this->phase = 0;
+    }
+}
+
+void Nemico::Shoot() {
+
+    if (this->isAlive) {   // logica per lo sparo e sarebbe da mettere un oggetto proiettile per permettere all'alieno di sparare alla navicella
+        // e qui serve la logica per lo sparo che e' solo grafica
+    }
 }
