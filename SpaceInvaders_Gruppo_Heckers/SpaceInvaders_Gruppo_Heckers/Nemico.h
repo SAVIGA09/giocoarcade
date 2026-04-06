dffb795af2c2d0ed8d0702ebd027fea1a53b24c3
Gabriele Amore <amore.gabriele090409@gmail.com>
2026-04-06 10:41:02 +0200

    classe nemico finita con implementazione in base alla presentazione

--- SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/Nemico.h
+++ SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/Nemico.h
@@ -1,33 +1,33 @@
 #pragma once
 #include <string>
 #include <windows.h>
-#include <gdiplus.h> //per trasformare i dati in qualcosa che si puo vedere a schermo
-using namespace std;
-using namespace Gdiplus;
+#include <gdiplus.h>//per trasformare i dati in qualcosa che si puo vedere a schermo 
 
-class Nemico
-{
-private: //dichiarazione variabili private
-	int x;
-	int y;
-	int width;
-	int height;
-	int phase;
-	string type;
-	Color color;
-	bool isAlive;
+using namespace std;
+using namespace Gdiplus; 
+
+class Nemico {
+private:
+    
+    int x, y;
+    int width, height;   // dati su cui si basa la creazione dell'alieno
+    int phase;
+    string type;
+    Color color;
+    bool isAlive;
 
 public:
-	//dichiarazione costruttore nemico e metodi da utilizzare e definire
-	Nemico();
-	Nemico(int x, int y, int height, int width, string type, Color color);
-	void GotHit();
-	bool VivoOMorto();
-	void SpostatiSinistra(int pixel);
-	void SpostatiDestra(int pixel);
-	void SpostatiDestra(int pixel);
-	void SpostatiSotto(int pixel);
-	void Spostati();
-	void ChangePhase();
-	void Spara();
+    
+    Nemico();  // costruttori e metodi che ho dichiarato
+    Nemico(int x, int y, int height, int width, string type, Color color);
+
+    void GotHit();   // stato
+    bool VivoOMorto(); 
+
+    void Spostati(int dx, int dy); //qua gli do di quanti "pixel" mi devo spostare e poi in base a quanti gliene do li aggiungera o togliera a x e y
+    void ChangePhase();    // azioni spiegate nel file che il programma deve fare e non chiedere all'utente
+    void Spara();  //che poi mi servira a me per l'alieno combinandolo anche con i proiettili
+
+    int getX();  // per valutare in che posizione sta l'alieno e anche per valutare se e' dentro il range dei bordi per il disegno
+    int getY(); // per valutare in che posizione sta l'alieno e anche per valutare se e' dentro il range dei bordi per il disegno
 };
--- SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/main.cpp
+++ SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/main.cpp
@@ -1,31 +1,50 @@
 #include <string>
 #include <windows.h>
-#include <gdiplus.h> //sempre per trasformare i dati in qualcosa che si puo vedere a schermo
+#include <gdiplus.h> //sempre per trasformare i dati in qualcosa che si puo vedere a schermo 
 #include "Nemico.h"
+
 using namespace std;
 using namespace Gdiplus;
 
-int main() {
-	Nemico n1 = Nemico(100, 100, 20, 20, "alieno", Color::Red);
-	Nemico n2 = Nemico(100, 200, 20, 20, "alieno", Color::Blue); //dichiarazione ipotetica degli alieni nemici
 
-	// inizia lo shift automatico degli alieni 
-	//il countspara parte da 0 perche non si sta iniziando a sparare e poi si aggiorna nel while
-	int countSpara = 0;
+int main() 
+{
+    int limiteDestro = 700; //modificabili in base al limite di "schermo" che voglio
+    int limiteSinistro = 0;
 
-	while (true) {
+   
+    Nemico n1 = Nemico(100, 100, 30, 30, "alieno", Color::Red);  //creo alieni
+    Nemico n2 = Nemico(160, 100, 30, 30, "alieno", Color::Red);
+    Nemico n3 = Nemico(220, 100, 30, 30, "alieno", Color::Red);
 
-		n1.SpostatiESpara();
-		n2.SpostatiESpara();
+    int spostamentoX = 10; //di base l'ho messo cosi che poi viene aggiunto alle posizioni attuali da dove partono gli alieni in questo caso 100 o 160 o 220 + 10 poi se lo richiamo ancora +10 eccettera
+    int spostamentoY = 20; //di base l'ho messo cosi che poi viene aggiunto alle posizioni attuali da dove partono gli alieni in questo caso 100 + 10 poi se lo richiamo ancora +10 eccettera
 
-		Sleep(100);//si rallenta il ciclo 
+    while (true)
+    {
 
-		//si continua lo shift delle posizione degli alieni in concomitanza al fatto che sparano colpi
+        if (n1.getX() > limiteDestro || n1.getX() < limiteSinistro || n2.getX() > limiteDestro || n2.getX() < limiteSinistro || n3.getX() > limiteDestro || n3.getX() < limiteSinistro)  //controllo i bordi se gli alieni stanno uscendo o meno perche se stanno uscendo devo farli scendere di un pochino verso il basso
+        {
+            spostamentoX = spostamentoX * -1;  //chiesto a gemini per come fare l'inversione dell'andamento dell'alieno essendo che non sapevo come farlo
 
-		countSpara++;
+            n1.Spostati(0, spostamentoY);   // faccio scendere tutti di 20 essendo che si arriva al bordo
+            n2.Spostati(0, spostamentoY);   
+            n3.Spostati(0, spostamentoY);
+        }
+        else
+        {
+            
+            n1.Spostati(spostamentoX, 0);
+            n2.Spostati(spostamentoX, 0);  // se ce spazio possono continuare a farli andare in laterale
+            n3.Spostati(spostamentoX, 0);
+        }
+       
+        n1.Spara();
+        n2.Spara();
+        n3.Spara();  // gestione sparo che poi devo implementare al proiettili di taha
 
-	}
-	//serve sfruttare il gothit e poi il successivo unalive per valutare se l'alieno viene eliminato o meno 
+        Sleep(200);
+    }
 
-	return 0;
+    return 0;
 }
