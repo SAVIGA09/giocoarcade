ef6cb3a9777e4bbf91937ba81a397368cb0371ad
tahathegamer51 <tahakharroubi09@gmail.com>
2026-04-07 14:27:17 +0200

    ho fatto il mio main per i proettili

---
+++ SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/main_proiettili.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include "proiettili.h"
+using namespace std;
+
+int main() {
+    // Creiamo un proiettile che parte da x=10, y=10 e va verso l'alto (direzione -1)
+    Proiettile p(10, 10, -1);
+
+    cout << "Partenza proiettile..." << std::endl;
+
+    // Lo facciamo muovere per 5 volte e vediamo dove va
+    for (int i = 0; i < 5; i++) {
+        p.aggiorna();
+        cout << "Posizione attuale Y: " << p.getY() << endl;
+        cout << "Posizione attuale X: " << p.getX() << endl;
+
+    }
+
+    return 0;
+}
