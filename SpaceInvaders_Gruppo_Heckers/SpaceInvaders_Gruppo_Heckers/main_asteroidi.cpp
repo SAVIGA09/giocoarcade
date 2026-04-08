05e7c33c28eb50cfa3846bda2effeb85d36a128b
mattia9900 <apuzzo.mattia01@gmail.com>
2026-04-08 16:55:52 +0200

    Update main_asteroidi.cpp

--- SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/main_asteroidi.cpp
+++ SpaceInvaders_Gruppo_Heckers/SpaceInvaders_Gruppo_Heckers/main_asteroidi.cpp
@@ -1,12 +1,28 @@
 #include <iostream>
 #include "asteroidi.h"
+
 using namespace std;
 
-int main()
-{
-	Asteroidi asteroide;
+int main() {
+    Asteroidi barriera;
 
-	
+    barriera.setCoordinate('A', 20, 10);
+    
+    
+    cout << "Sparo a (5, 5)" << endl;
+    if (barriera.asteroideColpito(5, 5)) {
+        cout << "Colpito!" << endl;
+    }
+    else {
+        cout << "Mancato!" << endl;
+    }
+ 
+    cout << "Sparo a (10, 20)..." << endl;
+    barriera.asteroideColpito(10, 20);
 
-	return 0;
+    barriera.setRotto();
+
+    cout << "Test completato. Se il contatore di Apuzzo funziona, dopo 2 colpi l'asteroide sparisce." << endl;
+
+    return 0;
 }
